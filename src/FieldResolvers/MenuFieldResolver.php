e157bd18a7226da12f5891615f3c08f83c610962
Leonardo Losoviz <leo@getpop.org>
2020-06-11 15:59:59 +0800

    Added non-nullable to fields

--- src/FieldResolvers/MenuFieldResolver.php
+++ src/FieldResolvers/MenuFieldResolver.php
@@ -37,6 +37,17 @@ class MenuFieldResolver extends AbstractDBDataFieldResolver
         return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
     }
 
+    public function isSchemaFieldResponseNonNullable(TypeResolverInterface $typeResolver, string $fieldName): bool
+    {
+        $nonNullableFieldNames = [
+            'items',
+        ];
+        if (in_array($fieldName, $nonNullableFieldNames)) {
+            return true;
+        }
+        return parent::isSchemaFieldResponseNonNullable($typeResolver, $fieldName);
+    }
+
     public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
     {
         $translationAPI = TranslationAPIFacade::getInstance();
