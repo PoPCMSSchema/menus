4b428221c9f60e07f4e8f3e3862603b055656d1a
leoloso <leo@getpop.org>
2021-11-25 15:05:27 +0000

    Merge pull request #1242 from leoloso/Migrate-from-FilterInputContainer-to-InputObject
    Migrate from FilterInputContainer to InputObject in all fields

---
+++ src/TypeResolvers/InputObjectType/MenuByInputObjectTypeResolver.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\Menus\TypeResolvers\InputObjectType;
+
+use PoP\ComponentModel\TypeResolvers\InputObjectType\AbstractOneofInputObjectTypeResolver;
+use PoP\Engine\TypeResolvers\ScalarType\IDScalarTypeResolver;
+
+class MenuByInputObjectTypeResolver extends AbstractOneofInputObjectTypeResolver
+{
+    private ?IDScalarTypeResolver $idScalarTypeResolver = null;
+
+    final public function setIDScalarTypeResolver(IDScalarTypeResolver $idScalarTypeResolver): void
+    {
+        $this->idScalarTypeResolver = $idScalarTypeResolver;
+    }
+    final protected function getIDScalarTypeResolver(): IDScalarTypeResolver
+    {
+        return $this->idScalarTypeResolver ??= $this->instanceManager->getInstance(IDScalarTypeResolver::class);
+    }
+
+    public function getTypeName(): string
+    {
+        return 'MenuByInput';
+    }
+
+    public function getTypeDescription(): ?string
+    {
+        return $this->getTranslationAPI()->__('Oneof input to specify the property and data to fetch a menu', 'menus');
+    }
+
+    public function getInputFieldNameTypeResolvers(): array
+    {
+        return [
+            'id' => $this->getIDScalarTypeResolver(),
+        ];
+    }
+
+    public function getInputFieldDescription(string $inputFieldName): ?string
+    {
+        return match ($inputFieldName) {
+            'id' => $this->getTranslationAPI()->__('Query by menu ID', 'menus'),
+            default => parent::getInputFieldDescription($inputFieldName),
+        };
+    }
+}
